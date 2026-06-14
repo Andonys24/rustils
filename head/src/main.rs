8ed42396f75d0794eeaa6a86f8b9532e300478f4
Andonys24 <andonihernandez1990@gmail.com>
2026-06-13 21:14:38 -0600

    perf: add dynamic -n flag parsing and reuse string buffer in head

--- head/src/main.rs
+++ head/src/main.rs
@@ -6,21 +6,68 @@ use std::{
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut args = env::args().skip(1);
-    let file_path = match args.next() {
-        Some(path) => path,
+    // We collected the arguments.
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    // Default values
+    let mut lines_to_read: usize = 10;
+    let mut file_path: Option<&String> = None;
+
+    // Argument Analysis
+    let mut i = 0;
+
+    while i < args.len() {
+        match args[i].as_str() {
+            "-n" => {
+                if i + 1 < args.len() {
+                    match args[i + 1].parse::<usize>() {
+                        Ok(num) => {
+                            lines_to_read = num;
+                            i += 2;
+                        }
+                        Err(e) => {
+                            eprintln!("head: invalid number of lines '{}': {}", args[i + 1], e);
+                            process::exit(1);
+                        }
+                    }
+                } else {
+                    eprintln!("head: option requires an argument -- 'n'");
+                    process::exit(1);
+                }
+            }
+            // Anything that does not begin with '-' will be assumed to be a file path
+            _path => {
+                file_path = Some(&args[i]);
+                i += 1;
+            }
+        }
+    }
+
+    // We validate that an argument exists
+    let path = match file_path {
+        Some(p) => p,
         None => {
             eprintln!("Error: Please provide a filepath.");
             process::exit(1);
         }
     };
-    let file = File::open(&file_path)?;
-    let reader = BufReader::new(file);
+
+    let file = File::open(path)?;
+    let mut reader = BufReader::new(file);
+
     let stdout = io::stdout();
     let mut handle = stdout.lock();
 
-    for line in reader.lines().take(10) {
-        writeln!(handle, "{}", line?)?;
+    let mut line_buffer = String::new();
+
+    for _ in 0..lines_to_read {
+        line_buffer.clear();
+
+        if reader.read_line(&mut line_buffer)? == 0 {
+            break;
+        }
+
+        write!(handle, "{}", line_buffer)?;
     }
 
     Ok(())
