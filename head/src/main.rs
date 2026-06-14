c0d4cdafebb0ca1f49814ff17aa32c4cdf031ec5
Andonys24 <andonihernandez1990@gmail.com>
2026-06-13 20:02:22 -0600

    refactor: clean up head code to be more idiomatic

--- head/src/main.rs
+++ head/src/main.rs
@@ -1,35 +1,27 @@
 use std::{
     env,
     fs::File,
-    io::{BufRead, BufReader},
+    io::{self, BufRead, BufReader, Write},
+    process,
 };
 
-fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = env::args().skip(1);
+    let file_path = match args.next() {
+        Some(path) => path,
+        None => {
+            eprintln!("Error: Please provide a filepath.");
+            process::exit(1);
+        }
+    };
+    let file = File::open(&file_path)?;
+    let reader = BufReader::new(file);
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
 
-    if args.is_empty() {
-        println!("Error: Please provider a filepath.");
-        return;
+    for line in reader.lines().take(10) {
+        writeln!(handle, "{}", line?)?;
     }
 
-    let file_path = &args[0];
-
-    match File::open(file_path) {
-        Ok(file) => {
-            let reader = BufReader::new(file);
-
-            for line_result in reader.lines().take(10) {
-                match line_result {
-                    Ok(line) => println!("{}", line),
-                    Err(e) => {
-                        eprintln!("head: error reading linea: {}", e);
-                        break;
-                    }
-                }
-            }
-        }
-        Err(e) => {
-            eprintln!("head: {}: {}", file_path, e);
-        }
-    }
+    Ok(())
 }
